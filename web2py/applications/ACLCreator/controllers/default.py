b25ae2adada38ac55458c9fffcb6ca97962242eb
Proger666 <knorra@mail.ru>
2017-04-19 01:25:20 +0300

    Ongoing

--- web2py/applications/ACLCreator/controllers/default.py
+++ web2py/applications/ACLCreator/controllers/default.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 ### required - do no delete
+from pip._vendor.requests.packages.urllib3.util import url
+
+
 def user(): return dict(form=auth())
 
 
@@ -14,17 +17,18 @@ def index():
     if request.vars.csv_file is not '' and len(request.vars) is not 0:
         db.t_cache.insert(f_data=request.vars.csv_file, f_name=request.vars.csv_file.filename)
         db.commit()
+        import csv
+        import urllib2
+        file = db(db.t_cache.f_name.like(request.vars.csv_file.filename)).select().first()
+        url = 'http://127.0.0.1:8000'+URL('download', args=file.f_data)
+        import pandas as pd
+        data = pd.read_csv(url)
         redirect(URL('default', 'graph', vars={'file_name':request.vars.csv_file.filename}))
     return locals()
 
 def graph():
     csv_file = db(db.t_cache.f_name.like(request.vars.file_name)).select().first()
-    import csv
-    request.args=csv_file.f_data
-    file = download()
-    spamreader = csv.reader(file, delimiter=',', quotechar='|')
-    for row in spamreader:
-        print row
+
     return locals()
 
 def error():
