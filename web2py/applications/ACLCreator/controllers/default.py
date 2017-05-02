aa9dc6eb7d04f979fb051034737489a641dc3fa2
Proger666 <knorra@mail.ru>
2017-05-02 14:18:27 +0300

    Ongoing

--- web2py/applications/ACLCreator/controllers/default.py
+++ web2py/applications/ACLCreator/controllers/default.py
@@ -3,11 +3,15 @@
 import datetime
 
 import StringIO
+
+import re
 from pip._vendor.requests.packages.urllib3.util import url
 import pandas as pd
 import csv
 import io
 from os import path
+import xlsxwriter
+
 
 def user(): return dict(form=auth())
 
@@ -34,7 +38,18 @@ def zones():
         sameIPhost = request.vars.maxH
         objPref = request.vars.objpref
         separator = '-'
-        ports = db(db.t_cache.f_name.like(request.vars.filename)).select(db.t_cache.f_ports).first().f_ports
+        zone_sep_f = '['
+        zone_sep_s = ']'
+        src_col = 'source.ip: Descending'
+        dst_col = 'dest.ip: Descending'
+        dstport_col = 'dest.port: Descending'
+        transport_col = 'transport: Descending'
+        seg_VM_col = 'VM'
+        seg_IP_col = 'Ip addres'
+        src_obj_name = object
+
+        # extract max ports that was saved in DB with data file
+        maxPorts = db(db.t_cache.f_name.like(request.vars.filename)).select(db.t_cache.f_ports).first().f_ports
         if hasattr(request.vars.segment_file, 'file'):
             f_id = str(datetime.datetime.now().microsecond) + request.vars.segment_file.filename
             db.t_cache.insert(f_data=request.vars.segment_file, f_name=f_id, f_ports=request.vars.ports)
@@ -47,40 +62,144 @@ def zones():
         sheets = xl.book.sheets()
         data = pd.read_csv(
             db.t_cache.f_data.retrieve(db(db.t_cache.f_name.like(request.vars.filename)).select().first().f_data)[1])
+
+        # Objects for ACL
+        objectGroup_network_tuple = {'obj_name': [],
+                                     'members': []}
+        objectGroup_service_tuple = {'obj_name': [],
+                                     'members': []}
+        objectPort_tuple = {'obj_name': ['http', 'https'],
+                            'value': ['80', '443']}
+        objectNetwork_tuple = {'obj_name': [],
+                               'type': [],
+                               'value': [],
+                               'description': []}
+
+        zone_rules_writer = []
+        # zone_rules = pd.DataFrame(zone_rules_writer.getvalue(), columns=['src.ip, dst.ip, dst.port'])
+        zone_rules = StringIO.StringIO()
+        xl_dataframe = pd.DataFrame(columns=[seg_VM_col, seg_IP_col, 'Zone_name'])
         for sheet in sheets:
-            zone_rules = StringIO.StringIO
-            zone_ips = [str(x) for x in sheet.col_values(1)]
-            # remove unicode and cast to STR
-            zone_name = str(sheet.name)
-            # filter by label source.ip and find all ip that is belongs to zone inside source DATA
-            source_tree = data[(data['source.ip: Descending'].isin(zone_ips) & (data['dest.port: Descending'] <= int(ports)))]
-            # Group by DEST IP - tree to DEST ip address so dest ip - list of all who interacte with it
-            dest_tree = source_tree.groupby(['dest.ip: Descending', 'dest.port: Descending', 'source.ip: Descending'],
-                                as_index=False).mean()
-            # group by dest ip and count non unique values (source ip) to count how much src ip connects to same dst and port
-            non_uniq_src_count = dest_tree.groupby(['dest.ip: Descending', 'dest.port: Descending'])['source.ip: Descending'].nunique()
-            for dest_port, hitcount in non_uniq_src_count.iteritems():
-                if hitcount >= sameIPhost:
-                    zone_rules.write(zone_name + separator +
-                                     zone_name + '_IP' + separator +
-                                     + dest_port[0] + separator + dest_port[1] + separator
-                                     + 'comment: number of hist for this dest ip and port = ' + hitcount + ',')
+            df_tmp = xl.parse(sheet.name)
+            df_tmp = df_tmp[[seg_VM_col, seg_IP_col]]
+            df_tmp['Zone_name'] = sheet.name
+            xl_dataframe = xl_dataframe.append(df_tmp)
+
+        # Tag everything with src zone and dst zone
+        for index, row in data.iterrows():
+           try:
+               src_z_name = str(
+                xl_dataframe.iloc[xl_dataframe.loc[xl_dataframe[seg_IP_col] == row[src_col]].index[0]]['Zone_name'])
+           except IndexError:
+               data.ix[index, 'src_zone_name'] = 'UNKNOWN'
+           else:
+               data.ix[index, 'src_zone_name'] = src_z_name
+           try:
+               dst_z_name = str(
+                   xl_dataframe.iloc[xl_dataframe.loc[xl_dataframe[seg_IP_col] == row[dst_col]].index[0]]['Zone_name'])
+           except IndexError:
+               data.ix[index, 'dst_zone_name'] = 'UNKNOWN'
+           else:
+               data.ix[index, 'dst_zone_name'] =  dst_z_name
+
+
+
+
+        # Create objects for all VMS from segment_file
+        for index, row in xl_dataframe.iterrows():
+            if row[seg_IP_col] not in objectNetwork_tuple['value']:
+                if validate_ip(row[seg_IP_col]):
+                    objectNetwork_tuple['value'].append(row[seg_IP_col])
                 else:
-                    temp_data = dest_tree.loc[dest_tree['dest.ip: Descending'] == '10.127.253.19', ['source.ip: Descending',
-                                                                                        'dest.ip: Descending',
-                                                                                        'dest.port: Descending']]
-                    for index, row in temp_data.iterrows():
-                        zone_rules.write(zone_name + separator +
-                                            row['source.ip: Descending'] + separator +
-                                         row['dest.ip: Descending']
+                    objectNetwork_tuple['value'].append('NOT ASSIGNED')
+                objectNetwork_tuple['obj_name'].append(
+                    objPref + zone_sep_f + row['Zone_name'] + zone_sep_s + '_' + re.sub(' ', '_', row[seg_VM_col]))
+                objectNetwork_tuple['description'].append(
+                    re.sub(' ', '_', row[seg_VM_col]) + ' from ' + row['Zone_name'])
+                objectNetwork_tuple['type'].append('host')
+
+        # Create objects for all uniq services
+        # Get all uniq pors via drop_duplicates and filter by maxPorts setting
+        uniq_ports = data.loc[data[dstport_col] < maxPorts, [transport_col, dstport_col]].drop_duplicates()
+        for index, row in uniq_ports.iterrows():
+            objectPort_tuple['obj_name'].append(row[transport_col] + '_' + str(row[dstport_col]))
+            objectPort_tuple['value'].append(
+                'service ' + row[transport_col] + ' destination eq ' + str(row[dstport_col]))
+        for zone_name in xl.book.sheet_names():
+            # create group object for zone_ip
+            objectNetwork_tuple['obj_name'].append('zone_' + zone_sep_f + zone_name + zone_sep_s + '_NET')
+            objectNetwork_tuple['type'].append('subnet')
+            objectNetwork_tuple['description'].append('Zone ' + zone_name + ' subnet')
+            objectNetwork_tuple['value'].append(zone_name + '_IP')
+
+            # Make objects dataframe
+            object_data = pd.DataFrame(objectNetwork_tuple)
+
+            zone_ips = xl_dataframe.loc[xl_dataframe['Zone_name'] == 'Main', seg_IP_col].tolist()
+            # remove unicode and cast to STR
+            zone_name = str(zone_name)
+            # filter by label source.ip and find all ip that is belongs to zone inside source DATA and dst not the same zone
+            source_tree = data[
+                (data[src_col].isin(zone_ips) & (data[dstport_col] <= int(maxPorts)) & (data['dst_zone_name'] != zone_name))]
+
+            source_tree['src_zone_name'] = zone_name
+            # Group by DEST IP - tree to DEST ip address so dest ip - list of all who interacte with it
+            dest_tree = source_tree.groupby(
+                [dst_col, dstport_col, src_col, transport_col, 'src_zone_name', 'dst_zone_name'],
+                as_index=False).mean()
+            # group by dest ip and count non unique values (source ip) to count how much src ip connects to same dst and port
+            aggregate_dst_hit = dest_tree.groupby(['dest.ip: Descending', 'dest.port: Descending', 'transport: Descending'])[
+                'source.ip: Descending'].nunique()
+            index = 1
+            object_data = pd.DataFrame(objectNetwork_tuple)
+            for dest_port, hitcount in aggregate_dst_hit.iteritems():
+                # Create src group obj
+                _tmp_src_grp_obj = objPref + zone_sep_f + zone_name + zone_sep_s + '_' + 'srv_' + str(index)
+
+                objectGroup_network_tuple['obj_name'].append(_tmp_src_grp_obj)
+                if row[src_col] not in objectNetwork_tuple['value']:
+                    if validate_ip(row[src_col]):
+                        objectNetwork_tuple['value'].append(row[src_col])
+                    else:
+                        objectNetwork_tuple['value'].append('NOT ASSIGNED')
+                    objectNetwork_tuple['obj_name'].append(
+                        objPref + zone_sep_f + row['Zone_name'] + zone_sep_s + '_' + row[seg_IP_col])
+                    objectNetwork_tuple['description'].append()
+                    objectNetwork_tuple['type'].append('host')
+
+                if hitcount < sameIPhost:
+                    abc = 1
+                else:
 
 
-
+                    grouped = dest_tree.loc[dest_tree[dst_col] == dest_port[0], [src_col]]
+                    for src_ip in grouped[src_col]:
+                        # Add src obj
+                        objectGroup_network_tuple['members'].append(
+                            str(object_data.iloc[object_data.loc[object_data['value'] == src_ip].index[0]]['obj_name']))
+                        zone_rules_writer.append('access-list ' + zone_name.capitalize() + '_in extended permit '
+                                                 + dest_port[2] + ' object-group ' + _tmp_src_grp_obj + ' object-group ' + )
 
     return locals()
 
+
+def validate_ip(s):
+    s = str(s)
+    a = s.split('.')
+    if len(a) != 4:
+        return False
+    for x in a:
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i < 0 or i > 255:
+            return False
+    return True
+
+
 def table():
-    data = pd.read_csv(db.t_cache.f_data.retrieve(db(db.t_cache.f_name.like(request.vars.filename)).select().first().f_data)[1])
+    data = pd.read_csv(
+        db.t_cache.f_data.retrieve(db(db.t_cache.f_name.like(request.vars.filename)).select().first().f_data)[1])
     ports = db(db.t_cache.f_name.like(request.vars.filename)).select(db.t_cache.f_ports).first()
     src_ip = data['source.ip: Descending'].unique()
     return locals()
@@ -93,13 +212,14 @@ def index():
         f_id = str(datetime.datetime.now().microsecond) + request.vars.csv_file.filename
         db.t_cache.insert(f_data=request.vars.csv_file, f_name=f_id, f_ports=request.vars.ports)
         db.commit()
-        redirect(URL('default', 'table', vars={'filename':f_id, 'ports':request.vars.ports}))
+        redirect(URL('default', 'table', vars={'filename': f_id, 'ports': request.vars.ports}))
     return locals()
 
 
 def graph():
     csv_file = db(db.t_cache.f_name.like(request.vars.filename)).select().first()
-    data = pd.read_csv(db.t_cache.f_data.retrieve(db(db.t_cache.f_name.like(request.vars.filename)).select().first().f_data)[1])
+    data = pd.read_csv(
+        db.t_cache.f_data.retrieve(db(db.t_cache.f_name.like(request.vars.filename)).select().first().f_data)[1])
     data = data.loc[(data['source.ip: Descending'] == request.vars.ip)
                     & (data['dest.port: Descending'] <= int(request.vars.ports)),
                     ['dest.ip: Descending', 'dest.port: Descending']]
@@ -111,12 +231,12 @@ def graph():
         # Write root
         spamwriter.writerow([root, ''])
         # write all nx2 matrix
-        for i in range(0,data.values.shape[0],1):
+        for i in range(0, data.values.shape[0], 1):
             x = data.values[i][0]
             if x is not prex:
                 spamwriter.writerow([root + ';' + str(x), ''])
             spamwriter.writerow(
-                    [root + ';' + x + ';' + (str(data.values[i][1]))])
+                [root + ';' + x + ';' + (str(data.values[i][1]))])
             prex = x
     return locals()
 
